52e259c36f2b9d378c170280bbcec1a51281c0c5
Jakob Helmecke <j.helmecke@gmail.com>
2021-10-09 21:23:38 +0200

    nvim: nvim-tree update on cd

--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -2,12 +2,15 @@ vim.g.nvim_tree_show_icons = { git = 0, folders = 1, files = 1, folder_arrows = 
 vim.g.nvim_tree_git_hl = 1
 vim.g.nvim_tree_special_files = {}
 vim.g.nvim_tree_root_folder_modifier = ':p:~'
+vim.g.nvim_tree_highlight_opened_files = 0
 vim.g.nvim_tree_icons = {
   default = '',
   symlink = '',
 }
 
-require('nvim-tree').setup {}
+require('nvim-tree').setup {
+  update_cwd = true,
+}
 
 vim.api.nvim_set_keymap('n', '<c-f>', '<cmd>NvimTreeToggle<cr>', { silent = true })
 vim.api.nvim_set_keymap('n', '<leader>l', '<cmd>NvimTreeFindFile<cr>', { silent = true })
